9b03901f542657c97e2f985f966a249d5726125f
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-02-06 11:17:40 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "Parser/Config.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 2)
+        exit(1);
+    Config cf(argv[1]);
+    cf.parse();
+    return 0;
+}
