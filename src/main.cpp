412e9e2b68c120b8ebafea59c72d7c1d897944f6
Bulak <bulak@Bulaks-iMac.local>
2023-02-16 23:22:01 +0100

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "../inc/Config.hpp"
+#include "../inc/Network.hpp"
 
 int main(int argc, char **argv) {
     if (argc != 2)
         exit(1);
     Config cf(argv[1]);
     cf.display();
+    // Network(cf).start();
     return 0;
 }
