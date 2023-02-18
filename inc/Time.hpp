1018794e05e51bc1ee72dc92985087ac8a7567d5
Bulak <bulak@Bulaks-iMac.local>
2023-02-18 21:57:18 +0100

    getTime

---
+++ inc/Time.hpp
@@ -0,0 +1,10 @@
+#ifndef TIME_HPP
+# define TIME_HPP
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+std::string getTime();
+
+#endif
---
+++ src/Time.cpp
@@ -0,0 +1,18 @@
+#include "../inc/Time.hpp"
+
+std::string getTime() {
+    // Get the current time point
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+
+    // Convert the time point to a time_t object
+    std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+
+    // Convert the time_t object to a struct tm object
+    std::tm* time_info = std::localtime(&current_time);
+
+    // Format the timestamp as a string
+    char time_str[100];
+    std::strftime(time_str, sizeof(time_str), "[%H:%M:%S]", time_info);
+
+    return time_str;
+}
