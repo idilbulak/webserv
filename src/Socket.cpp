7517e61a55191149dc8ac14f7a24b94e40979190
Daan Saat <daansaat@Daans-MacBook-Air.local>
2023-02-24 20:17:27 +0100

    tabs

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -11,17 +11,17 @@ Socket::Socket(std::string host, std::string port) :_host(host), _port(port) {
 	listen();
 }
 
-void	Socket::configure() {
+void Socket::configure() {
 
 	if (( _fd = socket( AF_INET, SOCK_STREAM, 0)) < 0) {
 		std::cout << "cannot create socket: " << std::string(strerror(errno)) << std::endl;
 		exit(1);
 	}
-    const int port = std::stoi(_port);
-    memset((char *)&_addr, 0, sizeof(_addr));
-    _addr.sin_family = AF_INET;
-    _addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    _addr.sin_port = htons(port);
+	const int port = std::stoi(_port);
+	memset((char *)&_addr, 0, sizeof(_addr));
+	_addr.sin_family = AF_INET;
+	_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	_addr.sin_port = htons(port);
 	int optval = 1;
 	setsockopt(_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 	fcntl(_fd, F_SETFL, O_NONBLOCK);
@@ -43,7 +43,7 @@ void Socket::listen() {
 	}
 }
 
-int	Socket::accept(struct kevent& event) {
+int Socket::accept(struct kevent& event) {
 
 	_fd = ::accept(event.ident, (struct sockaddr *)&_addr, &_addrlen); //_addrlen not initiated??
 	if (_fd < 0) {
