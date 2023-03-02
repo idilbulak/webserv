7672af59e30e0bedd33f28dd99b858ea0f7f20c5
ali-tevfik <tevfikdoner@hotmail.com>
2023-03-02 18:09:11 +0100

    fixed port 8080 and 4242

--- src/Request.cpp
+++ src/Request.cpp
@@ -20,21 +20,46 @@ std::vector<std::string> Request::split(std::string str, std::string delimiter) 
 }
 
 
+
+std::vector<std::string> split_crlf(std::string str) {
+    std::vector<std::string> words;
+    size_t pos = 0;
+    std::string delimiter = "\r\n";
+
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        std::string token = str.substr(0, pos);
+        if (!token.empty()) {
+            words.push_back(token);
+        }
+        str.erase(0, pos + delimiter.length());
+    }
+
+    if (!str.empty()) {
+        words.push_back(str);
+    }
+
+    return words;
+}
+
+
 Request::~Request(){}
 
 Request::Request(std::string buff){
-    std::vector<std::string> splitBuff = split(buff, "\n");
+
+    // std::cout << "buff is[" << buff<<"]" << std::endl << std::endl;
+    std::vector<std::string> splitBuff = split_crlf(buff);
     _reqMethod = split(splitBuff[0], " ")[0];
   
     _reqHost= split(split(splitBuff[1], " ")[1], ":")[0];
-    std::vector<std::string> findPort = split(split(splitBuff[1], " ")[1], ":");
-  
-    if (findPort.size() == 2)
-        _reqPort = findPort[1];
+    std::vector<std::string> findPort = split(splitBuff[1], ":");
+    // std::cout << splitBuff[1] << std::endl;
+
+    if (findPort.size() == 3)
+        _reqPort = findPort[2];
     else
         _reqPort = "80";
 
-    std::cout << "info start:" << std::endl << "method is " << _reqMethod <<  "\n host is " << _reqHost << "\n port is  " << _reqPort << std::endl;
+    std::cout << "info start:" << std::endl << "method is " << _reqMethod <<  "\n host is " << _reqHost << "\n port is[" << _reqPort << "]" << std::endl;
       
     
 
