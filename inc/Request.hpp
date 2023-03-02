b5af91da54ab52510817d5f9bb94db00fea2ff8d
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-03-02 09:50:39 +0100

    start

---
+++ inc/Request.hpp
@@ -0,0 +1,34 @@
+#ifndef REQUEST_HPP
+# define REQUEST_HPP
+
+#include <iostream>
+
+class Request {
+
+	public:
+		Request(std::string host, std::string port);
+		Request();
+		~Request(void);
+
+		// std::string getters() {return _;} ??
+
+	private:
+        std::string _request; //tum bufferi buraya koysak? acaba isimize yarar mi? gereksiz
+        //GET / HTTP/1.1
+        std::string _reqMethod; // GET
+        std::string _reqUrl; //buna gerek olmayabilir "http:" "//" host [ ":" port ] [ abs_path [ "?" query ]]
+        std::string _reqHost;
+        std::string _reqPort;
+        std::string _reqUri; //yukardaki abs_path
+        std::string _reqVersion; // HTTP/1.1
+//bizim bastirdigimiz header da body yok simdilik  bunu yapalim. body eklememiz lazim POST icin gerekli
+        std::string _reqBody;
+//mesela acaba burda bir sikinti oldugunda direk bir error pageine mi yonlendirmeliyiz ona bakalim.
+
+
+};
+
+std::ostream& operator<<(std::ostream &os, Request& obj);
+
+
+#endif
---
+++ src/Request.cpp
@@ -0,0 +1,6 @@
+#include "../inc/Request.hpp"
+
+// std::string _request += buff;
+// std::string _request += buff;
+// std::string _request += buff;
+// std::string _request += buff;
