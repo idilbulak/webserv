3642e11bcf9105c5c910ec028d8e8760b1c9f619
ali-tevfik <tevfikdoner@hotmail.com>
2023-03-22 11:00:20 +0100

    conf file check

--- inc/CheckConfFile.hpp
+++ inc/CheckConfFile.hpp
@@ -17,13 +17,17 @@ public:
 
 
     void checkBrackets(std::vector<std::string> tokens);
-
+    bool checkServerKey(std::string word);
+    bool checkLocationKey(std::string word);
+    void checkKey(const std::string path);
+    std::vector<std::string> split(std::string str, std::string delimiter) ;
 
     //getter setter
     void setServerBrackets(bool isOpen);
     void setLocationBrackets(bool isOpen);
     bool getLocationBrackets();
     bool getServerBrackets();
+
 };
 
 
