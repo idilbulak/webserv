ee7a7c3302d4ac68f863137b8fe3d8f868c4fd9b
Bulak <bulak@Bulaks-iMac.local>
2023-03-28 00:02:06 +0200

    req parse done

--- inc/HttpRequest.hpp
+++ inc/HttpRequest.hpp
@@ -11,67 +11,53 @@
 #include <vector>
 #include <map>
 #include "Config.hpp"
-#include "Time.hpp"
 
-#define CRFL "\r\n"
+#define CRLF "\r\n"
 
+struct Uri {
+	std::string	fullPath;
+	std::string	path;
+	std::string	queryStr;
+	std::string	protocol;
+	std::string	host;
+	std::string	port;
+	Uri() : fullPath(""), path(""), queryStr(""), protocol(""), host(""), port("") {}
+};
 
 class HttpRequest {
 	public:
 		HttpRequest();
-		HttpRequest(std::string buff);
+		HttpRequest(std::string buff, std::string port);
 		~HttpRequest(void);
 
-		bool isComplete(std::string buff);
-		
-		std::string	getPort();
-		std::string	getUri();
-		std::string	getMethod();
-		std::string	getBody();
-		std::string getContent();
-		std::string getCntDisposition();
-		std::string getCntType();
-		std::string getCntName();
-		std::string getCntFileName();
-		std::string getFileToDelete();
-		std::string getQueryStr();
-		std::string getVersion();
+		void	parseHeader();
+		void	parseBody();
+		Uri		parseUri(std::string token);
 
-		
-		std::string findBody(std::string buff);
-		void		findUri(std::string str);
-		void		findFileToDelete();
-		void parseBody();
-		std::string find(const std::string& buff, std::string lookfor, std::string end);
-		std::string getContentBetweenBoundaries(std::string &str, std::string &boundary);
-		std::vector<std::string> split_crlf(std::string str);
-		std::vector<std::string> split(std::string str, std::string delimiter);
+		bool						isComplete(std::string buff);
+		std::string 				getFirstLine();
+		std::string 				find(const std::string& buff, std::string lookfor, std::string end);
+		std::vector<std::string>	getLines();
+
+		std::string		getMethod();
+		std::string		getPort();
+		std::string		getVersion();
+		std::string		getBody();
+		std::string		getQueryStr();
+		Uri				getUri();
 
 	private:
-		std::string _method;
-		std::string _host;
-		std::string _port;
-		std::string _uri;
-		std::string _version;
-		std::string _type;
-		std::string	_queryStr;
-		std::string _boundry;
-		std::string _body;
-		std::string _content;
-		std::string _cntDisposition;
-		std::string _cntType;
-		std::string _cntName;
-		std::string _cntLength;
-		std::string _cntFileName;
-		std::string _fileToDelete;
-		// VirtualServer   _server;
-		// Location        _loc;
-		// std::string     _indxFile;
-		// int			    _cgiOn;
-		// HttpRequest      _req;
-		// Response        _res;
-		// std::map<int, std::string> _codeMap;
-		// std::map<std::string, std::string> _env;
+		std::string		_buff;
+		std::string		_method;
+		std::string		_port;
+		std::string		_version;
+		std::string		_body;
+		Location        _loc;
+		Uri				_uri;
+		std::map<std::string, std::string> 	_headers;
 };
+
+std::vector<std::string> split(std::string str, char delimiter);
+std::vector<std::string> splitFromCrlf(std::string str);
 
 #endif
